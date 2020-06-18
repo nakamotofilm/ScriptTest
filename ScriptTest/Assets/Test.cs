df10d058cf6078f0c40f65615ec3babcf883ef7c
中元 雄 <info@nakamotofilm.com>
2020-06-18 16:53:03 +0900

    lesson4

---
+++ ScriptTest/Assets/Test.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour
+{
+
+	void Start()
+	{
+		int[] array = { 10, 20, 30, 40, 50 };
+
+		Debug.Log("通常の順番");
+		for (int i = 0; i < array.Length; i++)
+		{
+			Debug.Log(array[i]);
+		}
+
+		Debug.Log("逆の順番");
+        for (int i = 4; i > -1; i--)
+        {
+			Debug.Log(array[i]);
+		}
+	}
+
+	void Update() {
+
+	}
+}
